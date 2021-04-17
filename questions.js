44b7d74f175ed7c870d758ef5d79f9ab3e78413f
ZancaaniDev <65592754+zanco-tech@users.noreply.github.com>
2021-04-17 17:32:49 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qofki ugu hor rumeeyey rasuulka nnkh xaga  caruurta waa?",
+    answer: "Cali Binu AbiDalib",
+    options: [
+      "Abu Bkar Sidiiq",
+      "Cusman Binu Cafaan",
+      "Cali Binu AbiDalib",
+      "Subeyr Binu Cawaam"
+    ]
+  },
+    {
+    numb: 2,
+    question: "ninki ugu horeyay ee musliiminta tujiyey salaad jimco waa?",
+    answer: "Sacad Binu Surara",
+    options: [
+      "Xudeyfa Binu Yaman",
+      "Cumar binu Khadaab",
+      "Sacad Binu Surara",
+      "Abu Bakar Sidiiq"
+    ]
+  },
+    {
+    numb: 3,
+    question: "sheeg sanadki u soo islamay saxabigi weynaa ee cumar binu khadab?",
+    answer: "Sanadki 6aad ee Nabinimada Rasuulka nnkh",
+    options: [
+      "Sanadki 6aad ee Nabinimada Rasuulka nnkh",
+      "Sanadki 2aad ee Nabinimada Rasuulka nnkh",
+      "Sanadki 3aad ee hijriga Rasuulka nnkh",
+      "Sanadki 6aad hijriga Rasuulka nnkh"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Wadanka ugu badan musliiminta aduunka waa?",
+    answer: "Indonesia",
+    options: [
+      "Saudi Arabia",
+      "Indonesia",
+      "Iran",
+      "India"
+    ]
+  },
+    {
+    numb: 5,
+    question: "wuxu dhacay dagaalki beder sandki ?",
+    answer: "17ki ramdaan sanadki 2aad ee hijriga",
+    options: [
+      "17 ki ramdan sanadki 3aad ee hijriga",
+      "23 ramdaan sanadki 2aad hijriga",
+      "12 ramdaan sanadki 1aad ee hijriga",
+      "17ki ramdaan sanadki 2aad ee hijriga"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
